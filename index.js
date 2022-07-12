f678291560f6a40921bab70e35921d03b10357b1
Sora Nai <mhtajan@trends.com.ph>
2022-07-12 17:30:01 +0800

    can now download csv

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const platformClient = require("purecloud-platform-client-v2");
-const http = require('http');
-const fs = require('fs');
+const download = require("download");
+const fs = require("fs");
+const fetch = require("node-fetch");
+const creds = require("./creds.json")
 const client = platformClient.ApiClient.instance;
 client.setEnvironment("mypurecloud.jp"); // Genesys Cloud region
 
-const clientID = "588c86a0-c120-4c47-9c00-99c113db00ca";
-const clientSecret = "G12v6taK-_sOMJ7oht6I1i52urd8pMX8DXOYLR37Efk";
-
 client
-  .loginClientCredentialsGrant(clientID, clientSecret)
+  .loginClientCredentialsGrant(creds.id, creds.secret)
   .then(({ accessToken }) => {
-    //console.log(token);
 
     client.setAccessToken(accessToken);
 
@@ -23,51 +21,42 @@ client
 
     apiInstance
       .getAnalyticsReportingExports(opts)
-      .then((data) => {
-        ObjectData = Object.values(data)
-        ObjectData1 = ObjectData[0] 
-        ObjectCount1 = Object.values(ObjectData1) // number of entities
-        EntityElements =Object.keys(ObjectCount1[0]) //entities
-        EntityS = Object.values(ObjectCount1)
-        EntityV = Object.keys(EntityS)
-        EntityValues = Object.values(ObjectCount1[0])
-        var TopField = EntityElements.join() + "\n"
-        fs.writeFileSync("demo.csv", TopField)
-        console.log(Object.keys(EntityS[0]))
-     for (i = 0; i < ObjectCount1.length; i++) // entity element loop
-       {
-         
-         var dl_entity = Object.keys(EntityS[i])
-         var dstr_entity = Object.values(EntityS[i])
-         for(x=0;x<dl_entity.length; x++) //downloadurl loop
-           {
-             if (dl_entity[x]=="downloadUrl")
-         {
-           console.log(dstr_entity[x])
-          urlToPrint = dstr_entity[x]
-         }
-           }
-         for(z=0;z<dl_entity.length;z++) //id loop
-           {
-             if(dl_entity[z]=="id")
-             {
-               console.log(dstr_entity[z])
-             }
-           }
-         for(y=0;y<dl_entity.length;y++) //viewtype loop
-           {
-             if(dl_entity[y]=="viewType")
-             {
-               console.log(dstr_entity[y])
-             }
-           }
-       }
-             
+      .then((data) => {  
+         let entities = data['entities']
+    entities.forEach(entry=>{
+      const url = entry['downloadUrl'];
+          const rootpath = `./reports`;
+          const filepath = `./reports/${entry['viewType']}`;
+          if (!fs.existsSync(rootpath)) {
+            fs.mkdirSync(rootpath);
+          }
+          if (!fs.existsSync(filepath)) {
+            fs.mkdirSync(filepath);
+          }
+          const fileoption = {
+            filename: `${entry['viewType']}-${entry['id']}.csv`,
+          };
+          const options = {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              ContentType: `application/json`,
+            },
+          };
+           //datacollection
+         fetch(url, options).then(async(res) => {
+            const filelink = res.url;
+            await download(filelink, filepath, fileoption).then(() => {
+
+              console.log(`Complete Downloading -`, Object.values(fileoption));
+            });
+          });
+    })
+            
       })
       .catch((err) => {
         console.log("There was a failure calling getAnalyticsReportingExports");
         console.error(err);
-      });
+      });   
   })
   .catch((err) => {
     console.log("Invalid Credentials");
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+
+    Package dependencies
+    download": 8.0.0
+    node-fetch": 2.6.1
+    purecloud-platform-client-v2": 141.0.0
