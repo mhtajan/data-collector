16df81e973df8c9cf2e0dee8a6d77883524b98f7
mhtajan <108911700+mhtajan@users.noreply.github.com>
2022-07-15 18:19:58 +0800

    removed fs.existSync(), and status=failed
    fs.existSync is unnecessary since download will automatically create folder and file
    if(entry.status == failed) is unnecessary since there is already a if-else statement for complete and else will return all other statuses
    as of now I am not sure if there is a pending status so there will be no reiteration of download for now
    date filter written is for testing to see if it will return the datasource and its modified time
    lowdb is not yet needed for now
    scheduling of function/task is not yet written

--- index.js
+++ index.js
@@ -1,94 +1,77 @@
-const platformClient = require("purecloud-platform-client-v2");
-const download = require("download");
-const fs = require("fs");
-const fetch = require("node-fetch");
-const creds = require("./creds.json");
-const client = platformClient.ApiClient.instance;
-client.setEnvironment("mypurecloud.jp"); // Genesys Cloud region
+const platformClient = require("purecloud-platform-client-v2")
+const download = require("download")
+const fs = require("fs")
+const fetch = require("node-fetch")
+const creds = require("./creds.json")
+const client = platformClient.ApiClient.instance
 
+client.setEnvironment("mypurecloud.jp") // Genesys Cloud region
 client
   .loginClientCredentialsGrant(creds.id, creds.secret)
   .then(({ accessToken }) => {
-    client.setAccessToken(accessToken);
+    client.setAccessToken(accessToken)
 
-    let apiInstance = new platformClient.AnalyticsApi();
-
+    let apiInstance = new platformClient.AnalyticsApi()
     let opts = {
       pageNumber: 1, // Number | Page number
       pageSize: 500, // Number | Page size
-    };
+    }
 
     apiInstance
       .getAnalyticsReportingExports(opts)
       .then(async (data) => {
         //logs for debug
-        const datareports = new console.Console(
-          fs.createWriteStream("./datareports.txt")
-        );
-        const urlreports = new console.Console(
-          fs.createWriteStream("./urlReports.txt")
-        );
-        const fail = new console.Console(fs.createWriteStream("./failure.txt"));
-        const log = new console.Console(fs.createWriteStream("./logs.txt"));
-        datareports.log(data);
-
-        let entities = data.entities;
+        const datalogs = new console.Console(fs.createWriteStream("./data.txt"))
+        const fail = new console.Console(fs.createWriteStream("./failure.txt"))
+        const log = new console.Console(fs.createWriteStream("./logs.txt"))
+        datalogs.log(data)
+        let entities = data.entities
         //loop entities
         entities.forEach((entry) => {
           //yesterdaydate
-          let date = new Date();
-          date.setDate(date.getDate() - 1);
-          yesterday = date.toISOString().split("T")[0];
-          const url = entry.downloadUrl;
-
-          if (entry.status == "FAILED") {
-            console.log("failure");
-          }
-          const rootpath = `./reports`;
-          const filepath = `./reports/${entry.viewType}`;
-          if (!fs.existsSync(rootpath)) {
-            fs.mkdirSync(rootpath);
-          }
-          if (!fs.existsSync(filepath)) {
-            fs.mkdirSync(filepath);
-          }
+          let date = new Date()
+          let current = new Date()
+          current.getDate()
+          current.setHours(current.getHours() + 8) //test convert from utc to phtime
+          date.setDate(date.getDate() - 1)
+          yesterday = date.toISOString().split("T")[0]
+          const url = entry.downloadUrl
+          const filepath = `./reports/${entry.viewType}`            
           const fileoption = {
             filename: `${entry.viewType}-${entry.id}.csv`,
-          };
+          }
           const options = {
             headers: {
               Authorization: `Bearer ${accessToken}`,
               ContentType: `application/json`,
             },
-          };
+          }
           //datefilter
           if (entry.modifiedDateTime.includes(yesterday)) {
-            log.log(entry.viewType, "\n" + entry.modifiedDateTime);
-            log.log(yesterday);
+            log.log(entry.viewType, "\n" + entry.modifiedDateTime)
+            log.log(yesterday)
+            log.log(current)
           }
           if (entry.status.includes("COMPLETED")) {
             // datacollection
             fetch(url, options).then(async (res) => {
-              const filelink = res.url;
+              const filelink = res.url
               await download(filelink, filepath, fileoption).then(() => {
-                console.log(
-                  `Complete Downloading -`,
-                  Object.values(fileoption)
-                );
-              });
-            });
+                console.log(`Complete Downloading -`, Object.values(fileoption))
+              })
+            })
           } else {
-            //status = failed
-            fail.log(entry);
+            //status failed or pending/(etc)
+            fail.log(entry)
           }
-        });
+        })
       })
       .catch((err) => {
-        console.log("There was a failure calling getAnalyticsReportingExports");
-        console.error(err);
-      });
+        console.log("There was a failure calling getAnalyticsReportingExports")
+        console.error(err)
+      })
   })
   .catch((err) => {
-    console.log("Invalid Credentials");
-    throw new Error(err);
-  });
+    console.log("Invalid Credentials")
+    throw new Error(err)
+  })
