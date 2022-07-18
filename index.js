b3a94eb7f963546a21aa4fa9e2c344851b369468
mhtajan <108911700+mhtajan@users.noreply.github.com>
2022-07-18 15:50:14 +0800

    added recursion, fixed date filtering
    created function for getting data and getting report for recursion of page counting
    adjusted date filtering (since it was already working)

--- index.js
+++ index.js
@@ -7,71 +7,89 @@ const client = platformClient.ApiClient.instance
 
 client.setEnvironment("mypurecloud.jp") // Genesys Cloud region
 client
-  .loginClientCredentialsGrant(creds.id, creds.secret)
-  .then(({ accessToken }) => {
-    client.setAccessToken(accessToken)
+	.loginClientCredentialsGrant(creds.id, creds.secret)
+	.then(({
+		accessToken
+	}) => {
+		client.setAccessToken(accessToken)
 
-    let apiInstance = new platformClient.AnalyticsApi()
-    let opts = {
-      pageNumber: 1, // Number | Page number
-      pageSize: 500, // Number | Page size
-    }
+		let apiInstance = new platformClient.AnalyticsApi()
+		let opts = {
+			pageNumber: 1, // Number | Page number
+			pageSize: 20, // Number | Page size
+		}
+		getAnalyticsReportingExports()
 
-    apiInstance
-      .getAnalyticsReportingExports(opts)
-      .then(async (data) => {
-        //logs for debug
-        const datalogs = new console.Console(fs.createWriteStream("./data.txt"))
-        const fail = new console.Console(fs.createWriteStream("./failure.txt"))
-        const log = new console.Console(fs.createWriteStream("./logs.txt"))
-        datalogs.log(data)
-        let entities = data.entities
-        //loop entities
-        entities.forEach((entry) => {
-          //yesterdaydate
-          let date = new Date()
-          let current = new Date()
-          current.getDate()
-          current.setHours(current.getHours() + 8) //test convert from utc to phtime
-          date.setDate(date.getDate() - 1)
-          yesterday = date.toISOString().split("T")[0]
-          const url = entry.downloadUrl
-          const filepath = `./reports/${entry.viewType}`            
-          const fileoption = {
-            filename: `${entry.viewType}-${entry.id}.csv`,
-          }
-          const options = {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-              ContentType: `application/json`,
-            },
-          }
-          //datefilter
-          if (entry.modifiedDateTime.includes(yesterday)) {
-            log.log(entry.viewType, "\n" + entry.modifiedDateTime)
-            log.log(yesterday)
-            log.log(current)
-          }
-          if (entry.status.includes("COMPLETED")) {
-            // datacollection
-            fetch(url, options).then(async (res) => {
-              const filelink = res.url
-              await download(filelink, filepath, fileoption).then(() => {
-                console.log(`Complete Downloading -`, Object.values(fileoption))
-              })
-            })
-          } else {
-            //status failed or pending/(etc)
-            fail.log(entry)
-          }
-        })
-      })
-      .catch((err) => {
-        console.log("There was a failure calling getAnalyticsReportingExports")
-        console.error(err)
-      })
-  })
-  .catch((err) => {
-    console.log("Invalid Credentials")
-    throw new Error(err)
-  })
+		function getAnalyticsReportingExports() {
+			apiInstance
+				.getAnalyticsReportingExports(opts)
+				.then(async (data) => {
+					//logs for debug
+					const datalogs = new console.Console(fs.createWriteStream("./data.txt"))
+					const fail = new console.Console(fs.createWriteStream("./failure.txt"))
+					const log = new console.Console(fs.createWriteStream("./logs.txt"))
+					datalogs.log(data)
+					let entities = data.entities
+
+					function getData() {
+						//loop entities
+						entities.forEach((entry) => {
+							//yesterdaydate
+							let date = new Date()
+							let current = new Date()
+							current.getDate()
+							current.setHours(current.getHours() + 8) //test convert from utc to phtime
+							date.setDate(date.getDate() - 1)
+							yesterday = date.toISOString().split("T")[0]
+							const url = entry.downloadUrl
+							const filepath = `./reports/${entry.viewType}`
+							const fileoption = {
+								filename: `${entry.viewType}-${entry.id}.csv`,
+							}
+							const options = {
+								headers: {
+									Authorization: `Bearer ${accessToken}`,
+									ContentType: `application/json`,
+								},
+							}
+							//datefilter
+							if (entry.modifiedDateTime.includes(yesterday)) {
+								if (entry.status.includes("COMPLETED")) {
+									// datacollection
+									fetch(url, options).then(async (res) => {
+										const filelink = res.url
+										await download(filelink, filepath, fileoption).then(() => {
+											console.log(`Complete Downloading -`, Object.values(fileoption))
+										})
+									})
+								} else {
+									//status failed or pending/(etc)
+									fail.log(entry)
+								}
+							}
+						})
+					}
+        
+					if (data.pageCount == data.pageNumber) {
+						getData()
+						console.log(data)
+						log.log(data)
+					} else {
+						getData()
+						console.log(data)
+						console.log(data.pageNumber)
+						opts.pageNumber = opts.pageNumber + 1
+						console.log(opts)
+						getAnalyticsReportingExports()
+					}
+				})
+				.catch((err) => {
+					console.log("There was a failure calling getAnalyticsReportingExports")
+					console.error(err)
+				})
+		}
+	})
+	.catch((err) => {
+		console.log("Invalid Credentials")
+		throw new Error(err)
+	})
