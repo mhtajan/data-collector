64b435cfc5d557b486a14ce75cc581b868666b42
mhtajan <108911700+mhtajan@users.noreply.github.com>
2022-07-20 11:04:17 +0800

    fixed and updated index.js
    change if(pageCount==pageNumber) to if(pageCount>=pageNumber)
    insert entity loop with download inside if(pageCount>=pageNumber) to avoid duplicate downloads
    change timestamping from using Date() to Date.now() method
    removed function getData(), this causes duplicate and ratelimit of api as well, also makes the script unresponsive
    added logs for failed exports

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf } = format;
 const client = platformClient.ApiClient.instance
 
-
 client.setEnvironment("mypurecloud.jp") // Genesys Cloud region
 client
     .loginClientCredentialsGrant(creds.id, creds.secret)
     .then(({ accessToken }) => {
         client.setAccessToken(accessToken)
-
+        let date = new Date()
+        let i = 0
+        date.setDate(date.getDate() - 1)
+        yesterday = date.toISOString().split("T")[0]
         let apiInstance = new platformClient.AnalyticsApi()
         let opts = {
             pageNumber: 1, // Number | Page number
@@ -24,53 +26,41 @@ client
             apiInstance
                 .getAnalyticsReportingExports(opts)
                 .then(async (data) => {
-                    let entities = data.entities
-
-                    function getData() {
-                        //loop entities
+                    let entities = data.entities                        
+                    //page && loop
+                    if (data.pageCount>=data.pageNumber) {
                         entities.forEach((entry) => {
-                            //yesterdaydate
-                            let date = new Date()
-                            let timestamp = new Date()
-                            timestamp.getDate()
-                            timestamp = timestamp.toISOString()
-                            date.setDate(date.getDate() - 1)
-                            yesterday = date.toISOString().split("T")[0]
-                            const filepath = `./reports/${entry.viewType}`
-                            const fileoption = {
-                                filename: `${entry.viewType}-${timestamp}.csv`,
-                            }
-                            const options = {
-                                headers: {
-                                    Authorization: `Bearer ${accessToken}`,
-                                    ContentType: `application/json`,
-                                },
-                            }
-                            //datefilter
-                            if (entry.modifiedDateTime.includes("2022")) {
-                                if (entry.status.includes("COMPLETED")) {
-                                    // datacollection
-                                    fetch(entry.downloadUrl, options).then(async (res) => {
-                                        const filelink = res.url
-                                        await download(filelink, filepath, fileoption).then(() => {
-                                            logger.info(`Complete Downloading - ${Object.values(fileoption)}`)
-                                        })
-                                    })
-                                } else {
-                                    //status failed or pending/(etc)
-                                    logger.info(`failed downloading ${entry.viewType} status: ${entry.status}`)
-                                }
-                            }
-                        })
-                    }
-                    //page
-                    if (data.pageCount == data.pageNumber) {
-                        getData()
-                    }
-                    else {
-                        getData()
-                        opts.pageNumber = opts.pageNumber + 1
-                        getAnalyticsReportingExports()
+                          //yesterdaydate                          
+                          const filepath = `./reports/${entry.viewType}`
+                          const fileoption = {
+                              filename: `${entry.viewType}-${Date.now()}.csv`,
+                          }
+                          const options = {
+                              headers: {
+                                  Authorization: `Bearer ${accessToken}`,
+                                  ContentType: `application/json`,
+                              },
+                          }
+                          //datefilter
+                          
+                          if (entry.modifiedDateTime.includes("2022")) {
+                              if (entry.status.includes("COMPLETED")) {
+                                  // datacollection
+                                  fetch(entry.downloadUrl, options).then(async (res) => {
+                                      const filelink = res.url
+                                      await download(filelink, filepath, fileoption).then(() => {
+                                          logger.info(`Complete Downloading - ${Object.values(fileoption)}`)
+                                      })
+                                  })
+                              } else {
+                                  //status failed or pending/(etc) 
+                                  fail_logger.debug(JSON.stringify(entry,null,2))
+                                  logger.info(`failed downloading ${entry.viewType} status: ${entry.status}`)  
+                              }
+                          }
+                      })
+                      opts.pageNumber = opts.pageNumber + 1
+                      getAnalyticsReportingExports()
                     }
                 })
                 .catch((err) => {
@@ -94,9 +84,10 @@ const logger = createLogger({
         timestamp(),
         myFormat
     ),
-    transports: [new transports.Console(),
+    transports: [
+    new transports.Console(),
     new transports.File({
-        filename: 'combined.log',
+        filename: 'app.log',
         level: 'info'
     }),
     new transports.File({
@@ -105,3 +96,14 @@ const logger = createLogger({
     })
     ]
 });
+const fail_logger = createLogger({
+    format: combine(
+        timestamp(),
+        myFormat
+    ),
+    transports: [new transports.File({
+        filename: 'debug.log',
+        level: 'debug'
+    })
+]
+})
