ef902f1a4648eed78cc82428377b9b5f6a5bfcc2
rnabingona12 <106578165+1rnabingona1@users.noreply.github.com>
2022-09-06 17:13:31 +0800

    reworked build without CRON
    reworked build without CRON

---
+++ src/ISO/ISO_maintenance_report.js
@@ -0,0 +1,36 @@
+const axios = require('axios').default
+const {
+  Parser,
+  transforms: { unwind, flatten },
+} = require('json2csv')
+const json2csvParser = new Parser({
+  transforms: [unwind({ blankOut: true }), flatten('__')],
+})
+const fs = require('fs')
+const path = require('path')
+const moment = require('moment')
+var datetime = moment().format('YYYY_MM_DD')
+const logger = require('../logger.js')
+const { workerData } = require('worker_threads')
+
+var MainteArr = []
+
+getMainteReport(workerData)
+function getMainteReport(body) {
+  axios({
+    method: 'get',
+    url:
+      'https://apps.mypurecloud.jp/platform/api/v2/audits/query/servicemapping',
+    headers: { Authorization: 'Bearer ' + body },
+  })
+    .then((response) => {
+      MainteArr.push(response.data)
+      const csv = json2csvParser.parse(MainteArr)
+      fs.writeFileSync(
+        `./ISO_reports/ISO_Maintenance_Report_${datetime}.csv`,
+        csv,
+      )
+      logger.info('ISO_Maintenance_Report EXPORTED SUCCESSFULLY!')
+    })
+    .catch((e) => logger.error(e))
+}
---
+++ src/main.js
@@ -0,0 +1,55 @@
+const isohandle = require('./ISO_handler.js')
+const fetch = require(`node-fetch`)
+const creds = require(`./creds.json`)
+const token = `${creds.id}:${creds.secret}`
+const encodedToken = Buffer.from(token).toString('base64')
+
+const params = new URLSearchParams()
+params.append('grant_type', 'client_credentials')
+
+function authenticate(){
+  fetch(`https://login.mypurecloud.jp/oauth/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${encodedToken}`,
+    },
+    body: params,
+  })
+    .then((res) => {
+      
+      if (res.ok) {
+        return res.json()
+      } else {
+        throw Error(res.statusText)
+      }
+    })
+    .then((jsonResponse) => {
+      isohandle(jsonResponse.access_token)
+      run(jsonResponse.access_token).catch(err => console.error(err))
+    })
+    .catch((e) => console.error(e))
+}
+
+
+  const { Worker } = require('worker_threads')
+  
+  function runService(workerData) {
+      return new Promise((resolve, reject) => {
+          const worker = new Worker(
+                  './handler.js', { workerData }); //thread for handler or viewtypes
+          worker.on('message', resolve);
+          worker.on('error', reject);
+          worker.on('exit', (code) => {
+              if (code !== 0)
+                  reject(new Error(
+  `Stopped the Worker Thread with the exit code: ${code}`));
+          })
+      })
+  }
+    
+  async function run(body) {
+      const result = await runService(body)
+      console.log(result);
+  }
+    
